52fb38c0c7c90c237cde0fd2001ab1f0ed149405
Rithanya-R2005 <rithanya.rgp@gmail.com>
2025-09-23 11:31:13 +0530

    Time: 2 ms (29.56%), Space: 43.5 MB (62.65%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,21 @@
 class Solution {
-    public void sub(int[] nums,List<List<Integer>> result,int i,List<Integer> temp){
+    public void sub(int[] nums,Set<List<Integer>> result,int i,List<Integer> temp){
         if(i==nums.length){
-            if(!result.contains(temp)){
-                result.add(new ArrayList<>(temp));
-            }
+            result.add(new ArrayList<>(temp));
             return;
         }
         temp.add(nums[i]);
         sub(nums,result,i+1,temp);
         temp.remove(temp.size()-1);
+        if(i<nums.length-1 && nums[i]==nums[i+1]) i=i+1;
         sub(nums,result,i+1,temp);
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
+        Set<List<Integer>> result=new HashSet<>();
         List<Integer> temp=new ArrayList<>();
         Arrays.sort(nums);
         sub(nums,result,0,temp);
-        return result;
+        List<List<Integer>> result2=new ArrayList<>(result);
+        return result2;
     }
 }
