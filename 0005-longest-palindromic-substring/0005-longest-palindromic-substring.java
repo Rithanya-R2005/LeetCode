355a163ba386b79c80668664b220b0365a0530a6
Rithanya-R2005 <rithanya.rgp@gmail.com>
2025-12-06 10:04:30 +0530

    Time: 15 ms (73.24%), Space: 43.5 MB (58.08%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -7,9 +7,9 @@ class Solution {
             int len1=expand(s,i,i);// finding odd length string is palindrome
             int len2=expand(s,i,i+1);// finding even length string is palindrome
             int len=Math.max(len1,len2);
-            if(len > end-start){// 
-                start=i-(len-1)/2;
-                end=i+len/2;
+            if(len > end-start){// to consider only longest palindromic substring
+                start=i-(len-1)/2; //here we are considering i at center position i-(len-1)/2 this will give value before i,len-1 because indexing starts from zero
+                end=i+len/2; //here i is at centre position i-len/2 gives value after i
             }
         }
         return s.substring(start,end+1);
