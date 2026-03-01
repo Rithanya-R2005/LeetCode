cdfa3c621f16b757e12ec89084ae3511933a2392
Rithanya-R2005 <rithanya.rgp@gmail.com>
2026-03-02 00:58:29 +0530

    Time: 4 ms (99.92%), Space: 47.2 MB (82.01%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int r=grid.length;
+        int c=grid[0].length;
+        int perimeter=0;
+        //See the constraint No:5
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    if(i==0 || (i>0 && grid[i-1][j]==0)){
+                        perimeter+=1;
+                    }
+                    if(i==r-1 || (i<r-1 && grid[i+1][j]==0)){
+                        perimeter+=1;
+                    }
+                    if(j==0 || (j>0 && grid[i][j-1]==0)){
+                        perimeter+=1;
+                    }
+                    if(j==c-1 || (j<c-1 && grid[i][j+1]==0)){
+                        perimeter+=1;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+}
