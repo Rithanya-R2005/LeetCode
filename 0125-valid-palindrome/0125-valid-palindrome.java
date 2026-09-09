6e313ce9264c26c6bf5c0e5a48425eae9dc3a8f7
Rithanya-R2005 <rithanya.rgp@gmail.com>
2026-09-09 11:34:39 +0530

    Time: 2 ms (99.31%), Space: 44.2 MB (73.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        // Because sb is muttable and string is imuttable.
-        String s1="";
-        for(int i=0;i<s.length();i++){
-            int a=sb.charAt(i);
-            if(a>=65 && a<=90){
-                a+=32;
-                char b=(char)a;
-                sb.setCharAt(i,b);
-            }
-            if((a>=97 && a<=122) || (a>=48 && a<=57)){ // 48 to 57 is a ascii character of number 0 to 9.
-                s1+=sb.charAt(i);
-            }
-        }
-        int start=0,end=s1.length()-1;
+        s=s.toLowerCase();
+        int start=0,end=s.length()-1;
         while(start<end){
-            if(s1.charAt(start)!=s1.charAt(end)){
-                return false;
+            char a=s.charAt(start);
+            char b=s.charAt(end);
+            if(!((a>='a' && a<='z') || (a>='0' && a<='9'))){
+                start++;
             }
-            start++;
-            end--;
+            if(!((b>='a' && b<='z') || (b>='0' && b<='9'))){
+                end--;
+            }
+            else{
+                if(a>='a' && a<='z' || a>='0' && a<='9'){
+                    if(a!=b){
+                        return false;
+                    }
+                    start++;
+                    end--;
+                }
+            }
         }
         return true;
     }
